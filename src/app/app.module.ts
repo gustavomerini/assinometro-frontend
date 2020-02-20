cb2d53e4c895ac62783339ee26a286558b71b8d2
gustavomerini <gutomerini@gmail.com>
2020-02-19 22:31:09 -0500

    Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    CoreModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: [AuthService],
+})
+export class CoreModule { }
