571f79dcaf295718c5c0091467bea24f8bac777c
gustavomerini <gutomerini@gmail.com>
2020-02-21 22:59:47 -0500

    Internationalization and Shared Module

--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RegisterComponent } from "./register.component";
 import { RegisterRoutingModule } from "./register-routing.module";
 
 @NgModule({
-  imports: [CommonModule, RegisterRoutingModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RegisterRoutingModule],
   exports: [],
   declarations: [RegisterComponent],
   providers: []
