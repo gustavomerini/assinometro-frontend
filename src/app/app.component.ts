7fdbfe55851cc3515e104bb5c3bfe6f2e69f7f43
Gustavo Merini <gustavo.merini@senior.com.br>
2020-08-10 09:50:54 -0300

    user locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Inject, LOCALE_ID } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 
 @Component({
@@ -11,8 +11,8 @@ export class AppComponent {
   public signedIn = false;
   public user = null;
 
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang("en");
+  constructor(@Inject(LOCALE_ID) private locale: string, private translate: TranslateService) {
+    this.translate.setDefaultLang('en-US');
   }
 
   public async verifyUserSession() {}
